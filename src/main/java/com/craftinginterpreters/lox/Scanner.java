759349c8f39fb3ce5fc3a9ed2e778b458327ff32
zhangran653 <zhangran42@126.com>
2023-07-08 18:47:21 +0800

    scanner tests

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -57,15 +57,44 @@ public class Scanner {
             }
             case '\n' -> line++;
             case '"' -> string();
-            default -> Lox.error(line, "unexpected character.");
+            default -> {
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, "unexpected character.");
+                }
+
+            }
         }
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        // Look for fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+            while (isDigit(peek())) {
+                advance();
+            }
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
         while (peek() != '"' && !isAtEnd()) {
             if (peek() == '\n') {
-                advance();
+                line++;
             }
+            advance();
         }
         if (isAtEnd()) {
             Lox.error(line, "unterminated string.");
@@ -96,6 +125,14 @@ public class Scanner {
             return '\0';
         }
         return source.charAt(current);
+    }
+
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) {
+            return '\0';
+        }
+        return source.charAt(current + 1);
     }
 
     private char advance() {
--- src/test/java/ScannerTest.java
+++ src/test/java/ScannerTest.java
@@ -5,10 +5,21 @@ public class ScannerTest {
 
     @Test
     public void test1() {
-        String source = "{}";
+        String source = "({})<= > != , ; +-*/= \"abe\"";
         Scanner scanner = new Scanner(source);
         for (var token : scanner.scanTokens()) {
             System.out.println(token);
         }
     }
+
+    @Test
+    public void test2() {
+        String source = "\"hello \n world!\"";
+        Scanner scanner = new Scanner(source);
+        for (var token : scanner.scanTokens()) {
+            System.out.println(token);
+        }
+    }
+
+
 }
