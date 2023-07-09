5b6be9f5f8baa71ed1ac9a11bdd27fc26c2ef904
zhangran653 <zhangran42@126.com>
2023-07-09 15:23:12 +0800

    parser

---
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,18 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression(){
+        return equality();
+    }
+
+
+}
